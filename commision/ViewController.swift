6b2c686f1399b086da58a4d08d441f5aa3abd70e
momohturay <turaysamorie@gmail.com>
2019-09-27 11:03:44 -0400

    Initial Commit

---
+++ commision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  commision
+//
+//  Created by Momoh Turay on 9/27/19.
+//  Copyright © 2019 kinGIndustries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
